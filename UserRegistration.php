500110575361f4af3d95d97e7ccf29b47ccc097a
TashrikZim <tanjiltashrikzim@gmail.com>
2025-03-14 01:25:32 +0600

    First Commit

---
+++ UserRegistration.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>User Registration</h2>
+    <form action="/home.php" method="post">
+      
+        <label for="fname">First Name: </lebel>
+        <input tyoe="text" id="fname" name="fname" placeholder="give your first name"> <br><br>
+        <label for="lname">Last Name : </label>
+        <input type="text" id="lname" name="lname" placeholder="give your last name"><br><br>
+        <label for="email">Email </label>
+        <input type="email"id="email" name="email" placeholder="abc@gmail.com"><br><br>
+       
+        <label for="pass" >Password : </label>
+        <input type="submit"> 
+
+</form>
+</body>
+</html>
