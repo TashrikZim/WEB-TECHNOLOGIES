42501bd0e084d6541827f55254d36556de3d1560
TashrikZim <tanjiltashrikzim@gmail.com>
2025-04-05 23:50:41 +0600

    Update Registration file and CSS

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <h1> You have successfully signed up! <h1>
+    <p>Click <a href="UserRegistration.php">Here </a> to return home </p>
+</center>
+</body>
+</html>
