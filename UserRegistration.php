e0c1059caa631c516db6833748655d0e1d7a41fd
TashrikZim <tanjiltashrikzim@gmail.com>
2025-04-05 23:47:08 +0600

    Update Registration file

--- UserRegistration.php
+++ UserRegistration.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="User.css" />
 </head>
 <body>
-    <center>
-<h2>User Registration</h2>
-    <form action="/home.php" method="post">
+    <center> 
+     
+        <h2>User Registration </h2>
+        <p>CAR</p>
+   <img src="../img/back.jpg" >  
+    <form action="dashboard.php" method="post">
       
         <label for="fname">First Name: </label>
-        <input tyoe="text" id="fname" name="fname" placeholder="give your first name"> <br><br>
+        <input type="text" id="fname" name="fname" placeholder="give your first name"> <br><br>
         <label for="lname">Last Name : </label>
         <input type="text" id="lname" name="lname" placeholder="give your last name"><br><br>
         <label for="email">Email :</label>
         <input type="email"id="email" name="email" placeholder="abc@gmail.com"><br><br>
        
         <label for="pass" >Password : </label>
-        <input type="password" id="pass" name="pass" ><br><br>
+        <input type="password" id="pass" name="pass" ><br><br> 
 
         <label for="phn"> Phone :</label>
         <input type="tel" id="phn" name="phn" placeholder="+880"><br><br>
@@ -42,16 +45,16 @@
             
      <label>Address: <textarea name="address" rows="3" placeholder="123st,10road,Dhaka"></textarea></label> <br><br>
 
-       <label for="dob"> Date of Birth : </label>
+       <label for="dob"> Date of Birth : </label> 
         <input type="date" id="dob" name="dob"><br><br>
 
         <label>Gender  :</label>
         <input type="radio" id="male" name="gender" value="Male">
-        <label>Male</label>
-        <input type="radio" id="female" name="gender" value="Female">
-        <label>Female</label>
+        <label class="gender" >Male</label>
+        <input type="radio" id="female"  name="gender" value="Female">
+        <label class="gender" >Female</label>
         <input type="radio" id="Others" name="gender" value="Others">
-        <label>Others</label> <br><br>
+        <label class="gender" >Others</label> <br><br>
 
      <label for="hobby"> Hobby : </label>
      <input type="checkbox" id="trv" name="hobby" value="Traveling">
@@ -62,8 +65,11 @@
      <label>Play Games </label>
      <input type="checkbox" id="oth" name="hobby" value="others">
      <label>Others </label>
-
-       <br><br> <input type="submit"> 
+       <br><br> 
+       <div>This project is a car rental management system designed to help users browse, book, and manage car rentals efficiently. It includes features for customer registration, viewing available cars, making reservations, tracking bookings, and handling payments. Admins can add/edit car details, manage rentals, and monitor system activity. The goal is to provide a user-friendly, responsive interface with real-time availability updates and secure booking functionality.
+       </div>
+       <input type="submit" class="btn submit" value="Submit"> 
+       <input type="reset" class="btn reset" value="Reset">
 
 </form>
 </center>
